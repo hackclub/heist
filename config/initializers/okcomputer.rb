409a5c0ab06b4c30617f45d424385e51ccae9d7c
Dhamari Trice-Hanson <39872667+dhamariT@users.noreply.github.com>
2026-04-01 21:28:32 -0400

    Guard OkComputer ferret check when FERRET_URL is not set

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,1 +1,3 @@
-OkComputer::Registry.register "ferret", OkComputer::HttpCheck.new(ENV.fetch("FERRET_URL"))
+if ENV["FERRET_URL"].present?
+  OkComputer::Registry.register "ferret", OkComputer::HttpCheck.new(ENV.fetch("FERRET_URL"))
+end
