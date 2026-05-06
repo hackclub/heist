871e1ec37ff2d408d5b124472c02fb3ecd3bbc02
Dhamari Trice-Hanson <39872667+dhamariT@users.noreply.github.com>
2026-05-06 17:29:28 -0400

    chore: refresh annotaterb schema comments on bulletin_post

--- app/models/bulletin_post.rb
+++ app/models/bulletin_post.rb
@@ -1,6 +1,28 @@
 # typed: false
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: bulletin_posts
+#
+#  id           :bigint           not null, primary key
+#  body         :text             not null
+#  discarded_at :datetime
+#  posted_at    :datetime         not null
+#  title        :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  author_id    :bigint
+#
+# Indexes
+#
+#  index_bulletin_posts_on_discarded_at  (discarded_at)
+#  index_bulletin_posts_on_posted_at     (posted_at)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (author_id => users.id) ON DELETE => nullify
+#
 class BulletinPost < ApplicationRecord
   include Discardable
 
