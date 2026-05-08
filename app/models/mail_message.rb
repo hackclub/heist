407be92213415ce5125f2602a78e731fe6b9a667
Dhamari Trice-Hanson <39872667+dhamariT@users.noreply.github.com>
2026-05-08 17:15:24 -0400

    chore: refresh annotaterb schema annotations
    Picks up the new users.hackatime_detected_at column and adds the
    schema header to mail_messages files that didn't have one yet.

--- app/models/mail_message.rb
+++ app/models/mail_message.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: mail_messages
+#
+#  id            :bigint           not null, primary key
+#  body          :text
+#  discarded_at  :datetime
+#  kind          :string           not null
+#  mailable_type :string
+#  read_at       :datetime
+#  subject       :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  mailable_id   :bigint
+#  user_id       :bigint           not null
+#
+# Indexes
+#
+#  index_mail_messages_on_discarded_at                   (discarded_at)
+#  index_mail_messages_on_mailable_type_and_mailable_id  (mailable_type,mailable_id)
+#  index_mail_messages_on_user_id                        (user_id)
+#  index_mail_messages_unread_per_user                   (user_id,read_at) WHERE (read_at IS NULL)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class MailMessage < ApplicationRecord
   include Discardable
 
